740eef245112a27180f1806f5bcf982ee03ad682
Aniruddh Nagare <codebyanii@gmail.com>
2025-06-26 01:45:29 +0530

    Array | Easy Q5 Q6 Solved

--- src/Array/Easy.java
+++ src/Array/Easy.java
@@ -17,10 +17,10 @@ public class Easy {
         int largest = arr[0];
         int secondLargest = -1;
         for (int j : arr) {
-            if(j > largest) {
+            if (j > largest) {
                 secondLargest = largest;
                 largest = j;
-            } else if (arr[j] < largest && arr[j] >secondLargest) {
+            } else if (arr[j] < largest && arr[j] > secondLargest) {
                 secondLargest = arr[j];
             }
         }
@@ -30,11 +30,11 @@ public class Easy {
     int secondSmallest(int[] arr) {
         int smallest = arr[0];
         int secondSmallest = Integer.MAX_VALUE;
-        for (int i: arr){
-            if(i> smallest){
+        for (int i : arr) {
+            if (i > smallest) {
                 smallest = secondSmallest;
                 secondSmallest = i;
-            }else if(i != secondSmallest && i <smallest){
+            } else if (i != secondSmallest && i < smallest) {
                 secondSmallest = i;
             }
         }
@@ -56,9 +56,82 @@ public class Easy {
         return true;
     }  //O(n)
 
+    int removeDuplicates(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
+    int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    int findFirstOc(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    int findLastOc(int[] nums, int target) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int[] searchRange(int[] nums, int target){
+        int first = findFirstOc(nums, target);
+        int last = findLastOc(nums, target);
+        return new int[]{first, last};
+    }
+
+    int[] rotateLeftByD(int[] nums, int d) {
+        int n = nums.length;
+        d = d % n; // handle d > n
+        int[] temp = new int[d];
+        // Step 1: Copy first d elements
+        for (int i = 0; i < d; i++) {
+            temp[i] = nums[i];
+        }
+        // Step 2: Shift remaining elements to the left
+        for (int i = d; i < n; i++) {
+            nums[i - d] = nums[i];
+        }
+        // Step 3: Copy temp elements to the end
+        for (int i = 0; i < d; i++) {
+            nums[n - d + i] = temp[i];
+        }
+        return nums;
+    }
+
+    int[] rotateLeftByOne(int[] nums) {
+        int temp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i - 1] = nums[i];
+        }
+        nums[nums.length - 1] = temp;
+        return nums;
+    }
 
     public static void main(String[] args) {
-
+        Easy easy = new Easy();
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(easy.rotateLeftByOne(arr)));
     }
 }
--- src/a0_Tests/Solution.java
+++ src/a0_Tests/Solution.java
@@ -1,24 +1,16 @@
 package a0_Tests;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
-    int removeDuplicates(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(Arrays.toString(new int[]{solution.removeDuplicates(new int[]{1,1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 8, 9})}));
+        int[] nums = {1,2,3,4,5,6,7,8};
     }
 }
 
